5db29fd8d8a8b886eeff9fb16d87c0edf3206037
zounon <mawussi.zounon@gmailcom>
2021-06-08 20:38:59 +0100

    add script to load larger matrices

---
+++ matrices/big/getLargerMatrices.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#Download matrices
+wget https://suitesparse-collection-website.herokuapp.com/MM/Schenk_AFE/af_shell10.tar.gz
+wget https://suitesparse-collection-website.herokuapp.com/MM/Schenk_AFE/af_shell2.tar.gz 
+wget https://suitesparse-collection-website.herokuapp.com/MM/Bourchtein/atmosmodd.tar.gz 
+wget https://suitesparse-collection-website.herokuapp.com/MM/Bourchtein/atmosmodl.tar.gz
+wget https://suitesparse-collection-website.herokuapp.com/MM/vanHeukelum/cage13.tar.gz
+wget https://suitesparse-collection-website.herokuapp.com/MM/Bodendiek/CurlCurl_2.tar.gz
+wget https://suitesparse-collection-website.herokuapp.com/MM/Dziekonski/dielFilterV2real.tar.gz
+wget https://suitesparse-collection-website.herokuapp.com/MM/Janna/Geo_1438.tar.gz
+wget https://suitesparse-collection-website.herokuapp.com/MM/Janna/Hook_1498.tar.gz
+wget https://suitesparse-collection-website.herokuapp.com/MM/Janna/ML_Laplace.tar.gz
+wget https://suitesparse-collection-website.herokuapp.com/MM/Schenk/nlpkkt80.tar.gz
+wget https://suitesparse-collection-website.herokuapp.com/MM/Janna/Serena.tar.gz
+wget https://suitesparse-collection-website.herokuapp.com/MM/PARSEC/Si87H76.tar.gz
+wget https://suitesparse-collection-website.herokuapp.com/MM/Janna/StocF-1465.tar.gz
+wget https://suitesparse-collection-website.herokuapp.com/MM/Janna/Transport.tar.gz
+
+
+#Unzip the files
+tar -xvf af_shell10.tar.gz
+tar -xvf af_shell2.tar.gz
+tar -xvf atmosmodd.tar.gz
+tar -xvf atmosmodl.tar.gz
+tar -xvf cage13.tar.gz
+tar -xvf CurlCurl_2.tar.gz
+tar -xvf dielFilterV2real.tar.gz
+tar -xvf Geo_1438.tar.gz
+tar -xvf Hook_1498.tar.gz
+tar -xvf ML_Laplace.tar.gz
+tar -xvf nlpkkt80.tar.gz
+tar -xvf Serena.tar.gz
+tar -xvf Si87H76.tar.gz
+tar -xvf StocF-1465.tar.gz
+tar -xvf Transport.tar.gz
+
+
